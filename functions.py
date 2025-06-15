0a983572da5469dadbe0a00d268361949a2fed17
Metin Kağıt <76729066+MetinKagit@users.noreply.github.com>
2025-06-15 19:49:42 +0300

    Fix Ace value adjustment in calculate_hand

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ def calculate_hand(cards):
     if sum(cards) == 21 and len(cards) == 2:
         return 0
 
-    if 11 in cards and sum(cards) > 21:
+    while 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
 
